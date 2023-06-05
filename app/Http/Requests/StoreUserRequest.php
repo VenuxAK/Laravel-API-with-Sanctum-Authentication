cbdc820a6a0759bd40273731fea87112af1da8c0
VenuxAK <anastasioak327@gmail.com>
2023-06-05 13:44:22 +0630

    API register,login and logout with sanctum authentication

---
+++ app/Http/Requests/StoreUserRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class StoreUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'name' => ['required', 'max:100'],
+            'email' => ['required', 'max:255', 'email', Rule::unique('users', 'email')],
+            'phone_no' => ['max:30'],
+            'role' => ['required', Rule::in(['programmer', 'designer'])],
+            'password' => ['required', 'confirmed', 'min:6', 'max:255']
+        ];
+    }
+}
